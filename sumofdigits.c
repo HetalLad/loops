54b7e0cf7ccca11affc6f08eeb315f8a25e4de2f
HetalLad <97840531+HetalLad@users.noreply.github.com>
2022-01-26 20:15:28 +0530

    Create sumofdigits.c

---
+++ sumofdigits.c
@@ -0,0 +1,17 @@
+int sum_digits(num)
+{
+    if (num==0)
+    return 0;
+    else 
+    return(num%10+sum_digits(num/10));
+}
+
+int main()
+{
+    int n,sum;
+   printf("Enter the number:");
+   scanf("%d",&n);
+   sum=sum_digits(n);
+   printf("The sum of digits is: %d",sum);
+   
+}
