2dbd41d4353927a9fbe1b787d1f7ac8e6ab25ee9
HetalLad <97840531+HetalLad@users.noreply.github.com>
2022-01-26 20:04:28 +0530

    Create pattern.c

---
+++ pattern.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int n=6;
+    int i,j;
+    for (i=1;i<=6;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("01");
+            
+        }
+        printf("\n");
+    }
+}
